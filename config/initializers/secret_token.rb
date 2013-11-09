f66fa675be025e82876532a96cbab01f0380efab
tokoku <jorge_gf_@hotmail.com>
2013-11-09 15:05:53 +0000

    primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ejercicio01Rails::Application.config.secret_key_base = '8119ed60cfd03332a0e03ebe547938b016b8a37bc4d5ef453057e0e05343dcab9d31e00f0e41dadc4ed6acec49c4f14fc985e10272a781627920a73dbb7edf16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ejercicio01Rails::Application.config.session_store :cookie_store, key: '_ejercicio01_rails_session'
